0476cddcdbf092f6a87a6bb27a58daeca91bb558
Shekhar Saha <saha.shekhar704@gmail.com>
2024-10-03 10:26:07 +0530

    application context aware

--- src/main/java/com/abc/CircularDependencyA.java
+++ src/main/java/com/abc/CircularDependencyA.java
@@ -1,20 +1,29 @@
 package com.abc;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+public class CircularDependencyA implements ApplicationContextAware, InitializingBean {
 
-public class CircularDependencyA {
-    public CircularDependencyA() {
-
-    }
     private CircularDependencyB circB;
+
+    private ApplicationContext context;
+
     public CircularDependencyB getCircB() {
         return circB;
     }
 
-    public void setCircB(CircularDependencyB circB) {
-        this.circB = circB;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        circB = context.getBean(CircularDependencyB.class);
     }
 
+    @Override
+    public void setApplicationContext(final ApplicationContext ctx) throws BeansException {
+        context = ctx;
+    }
 }
